94edb1f56ba9eed234b04aed5d4af2a833403864
Mack Cross <mack.cross@hotmail.com>
2020-08-09 15:50:57 +1200

    Move Grade enum into Flashcard class

--- Source/Flashcard.swift
+++ Source/Flashcard.swift
@@ -59,45 +59,47 @@ public class Flashcard: Equatable, Hashable {
         return lhs.uuid == rhs.uuid && lhs.word == rhs.word
     }
     
+    /**  Grades from SuperMemo Algorithm
+     * 0 - complete blackout.
+     * 1 - incorrect response; the correct one remembered
+     * 2 - incorrect response; where the correct one seemed easy to recall
+     * 3 - correct response recalled with serious difficulty
+     * 4 - correct response after a hesitation
+     * 5 - perfect response
+     */
+    public enum Grade: Int, CustomStringConvertible {
+        /// complete blackout.
+        case null
+        /// incorrect response; the correct one remembered
+        case bad
+        /// incorrect response; where the correct one seemed easy to recall
+        case fail
+        /// correct response recalled with serious difficulty
+        case pass
+        /// correct response after a hesitation
+        case good
+        /// perfect response
+        case bright
+
+        public var description: String {
+            switch self {
+            case .null:
+                return "complete blackout"
+            case .bad:
+                return "incorrect response; the correct one remembered"
+            case .fail:
+                return "incorrect response; where the correct one seemed easy to recall"
+            case .pass:
+                return "correct response recalled with serious difficulty"
+            case .good:
+                return "correct response after a hesitation"
+            case .bright:
+                return "perfect response"
+            }
+        }
+    }
+    
     
 }
 
-/**  Grades from SuperMemo Algorithm
- * 0 - complete blackout.
- * 1 - incorrect response; the correct one remembered
- * 2 - incorrect response; where the correct one seemed easy to recall
- * 3 - correct response recalled with serious difficulty
- * 4 - correct response after a hesitation
- * 5 - perfect response
- */
-public enum Grade: Int, CustomStringConvertible {
-    /// complete blackout.
-    case null
-    /// incorrect response; the correct one remembered
-    case bad
-    /// incorrect response; where the correct one seemed easy to recall
-    case fail
-    /// correct response recalled with serious difficulty
-    case pass
-    /// correct response after a hesitation
-    case good
-    /// perfect response
-    case bright
 
-    public var description: String {
-        switch self {
-        case .null:
-            return "complete blackout"
-        case .bad:
-            return "incorrect response; the correct one remembered"
-        case .fail:
-            return "incorrect response; where the correct one seemed easy to recall"
-        case .pass:
-            return "correct response recalled with serious difficulty"
-        case .good:
-            return "correct response after a hesitation"
-        case .bright:
-            return "perfect response"
-        }
-    }
-}
